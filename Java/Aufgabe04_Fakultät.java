424c5ad13a804965883416d189c69bb404976a50
Liberator28 <dailyelias11@gmail.com>
2026-03-22 15:28:36 +0100

    Update Aufgabe04_Fakultät.java

--- Java/Aufgabe04_Fakultät.java
+++ Java/Aufgabe04_Fakultät.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+// Aufgabe 4: Fakultät einer Zahl berechnen
+// Die Fakultät von 5 ist z.B. 1*2*3*4*5 = 120
+// 0 ist per Definition = 1
+public class Aufgabe04_Fakultaet {
+
+    public static long fakultaet(int n) {
+        long ergebnis = 1;
+        for (int i = 2; i <= n; i++) {
+            ergebnis = ergebnis * i;
+        }
+        return ergebnis;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Zahl eingeben: ");
+        int zahl = scanner.nextInt();
+
+        if (zahl < 0) {
+            System.out.println("Fehler: Fakultät gibt es nur für positive Zahlen!");
+        } else {
+            System.out.println(zahl + "! = " + fakultaet(zahl));
+        }
+   
+    }
+}
 
