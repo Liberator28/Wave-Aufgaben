a9716e1bd3961f475c511054a05ed805ae4b8deb
Liberator28 <dailyelias11@gmail.com>
2026-03-22 15:56:30 +0100

    Implement square numbers output from 1 to 20
    Add a program to print square numbers from 1 to 20.

--- Java/Aufgabe09_Quadratzahlen.java
+++ Java/Aufgabe09_Quadratzahlen.java
@@ -1,1 +1,11 @@
+// Aufgabe 9: Quadratzahlen von 1 bis 20 ausgeben
+
+public class Aufgabe09_Quadratzahlen {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i + " * " + i + " = " + (i * i));
+        }
+    }
+}
 
