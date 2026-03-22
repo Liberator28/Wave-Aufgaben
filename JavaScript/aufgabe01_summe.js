a3640319a2628a517b88d010c33d1f9d43f4f39c
Liberator28 <dailyelias11@gmail.com>
2026-03-22 16:33:33 +0100

    Add sum function with test cases in aufgabe01_summe.js

---
+++ JavaScript/aufgabe01_summe.js
@@ -0,0 +1,10 @@
+// JS-Aufgabe 1: Funktion die zwei Werte addiert
+
+function summe(a, b) {
+    return a + b;
+}
+
+// Testaufrufe
+console.log("Summe von 3 und 7: " + summe(3, 7));       
+console.log("Summe von -5 und 12: " + summe(-5, 12));
+console.log("Summe von 0.1 und 0.2: " + summe(0.1, 0.2)); 
