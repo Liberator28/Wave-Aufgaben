0e746867fd01d15222b1ae06b310fb42ce5e1851
Liberator28 <dailyelias11@gmail.com>
2026-03-22 15:25:41 +0100

    Enhance comments in Pascal's triangle implementation
    Added comments to clarify the purpose and example of Pascal's triangle.

--- Java/Aufgabe07_PascalschesDreieck.java
+++ Java/Aufgabe07_PascalschesDreieck.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * Aufgabe 7: Pascalsches Dreieck
  * Jede Zahl ist die Summe der beiden darüberliegenden Zahlen.
  * Die Ränder sind immer 1.
- *
+ 
  * Beispiel
  * 1
  * 1 1
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * 1 4 6 4 1
  * 1 5 10 10 5 1
  */
+
 public class Aufgabe07_PascalschesDreieck {
 
     public static void main(String[] args) {
@@ -28,6 +29,7 @@ public class Aufgabe07_PascalschesDreieck {
             // Jede Zeile hat i+1 Elemente
             dreieck[i] = new int[i + 1];
 
+            
             // Erstes und letztes Element jeder Zeile ist 1
             dreieck[i][0] = 1;
             dreieck[i][i] = 1;
@@ -36,6 +38,8 @@ public class Aufgabe07_PascalschesDreieck {
             for (int j = 1; j < i; j++) {
                 dreieck[i][j] = dreieck[i - 1][j - 1] + dreieck[i - 1][j];
             }
+       
+        
         }
 
         // Ausgabe
