d9e7fde11d3de2bbd702f7e6085b7239807bacc2
Liberator28 <dailyelias11@gmail.com>
2026-03-22 16:22:59 +0100

    Create abstract class Angestellter for employees
    Added abstract class Angestellter with methods for monthly and yearly pay.

--- Java/aufgabe13_polymorphismus/Angestellter.java
+++ Java/aufgabe13_polymorphismus/Angestellter.java
@@ -1,1 +1,30 @@
+/**
+ * Aufgabe 13: Polymorphismus
+ 
+ * Abstrakte Basisklasse für alle Angestellten
+ * - getMonthlyPay() ist abstrakt und wird in den Unterklassen implementiert.
+ * - getYearlyPay() ist bereits hier implementiert: 12 * Monatsgehalt.
+ */
+
+public abstract class Angestellter {
+
+    private String name;
+
+    public Angestellter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Abstrakte Methode: wird in Unterklassen konkret implementiert
+  
+    public abstract double getMonthlyPay();
+
+    // Jahresgehalt = 12 Monatsgehälter
+  
+    public double getYearlyPay() {
+        return getMonthlyPay() * 12;  }
+}
 
