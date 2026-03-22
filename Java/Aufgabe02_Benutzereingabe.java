9dffcf76bb7acaf7abcb5901baabeb987485e822
Liberator28 <dailyelias11@gmail.com>
2026-03-22 08:41:44 +0100

    Implement user input and greeting functionality

--- Java/Aufgabe02_Benutzereingabe.java
+++ Java/Aufgabe02_Benutzereingabe.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+// Aufgabe 2: Benutzereingabe einlesen und begrüßen
+public class Aufgabe02_Benutzereingabe {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Wie heißt du? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hallo " + name + "!");
+
+        scanner.close();
+    }
+}
 
